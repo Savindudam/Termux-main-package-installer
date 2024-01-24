b973be668aaa7a7041a50f565844383b11be361f
Savindu damsara <156694948+Savindudam@users.noreply.github.com>
2024-01-24 22:53:55 +0530

    Add files via upload
    To run this after cloning type this in termux 
    chmod +x main packages.sh
    ./main packages.sh

---
+++ main packages.sh
@@ -0,0 +1,10 @@
+echo "This script will install your basic packages "
+      Termux-setup-storage
+      pkg update && pkg upgrade-y
+      pkg install python-y
+      pkg install git-y
+      pkg install tmux-y
+      pkg install hollywood-y
+      pkg install nano-y
+      pkg install python2-y
+      pkg install python3-y
